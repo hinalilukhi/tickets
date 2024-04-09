22c7c5a5bcad7f785d5932183e4bb9e6da7a7c72
ShreeHari <hinali.lukhi@gmail.com>
2024-04-09 14:22:59 +0200

    backend APIS for the tickets

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -34,8 +34,11 @@ const start = async() => {
     if (!process.env.JWT_KEY) {
         throw new Error('JWT_KEY must be defined');
     }
+    if(!process.env.MONGO_URI){
+        throw new Error('MONGO URL must be defined');
+    }
     try{
-        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("connected");
     }catch(err){
         console.log(err);
